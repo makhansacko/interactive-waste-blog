b23d231504b46b33c3a515350fadcabc5c12d486
makhansacko <makhansacko@gmail.com>
2024-05-06 17:55:34 +0000

    Update ChartParagraph.svelte

--- src/lib/ChartParagraph.svelte
+++ src/lib/ChartParagraph.svelte
@@ -15,35 +15,37 @@
                 margin: 50px auto;
            }
            
-           p {
-                display: block;
+           p, h3 {
                 font-size: 1.5em;
                 line-height: 1.5em;
                 margin: 0 0 1em 0;
+                padding: 0; /* Ensure there's no additional padding affecting text layout */
+            }
+
+            h3 {
+                font-weight: bold; /* Ensuring headers are bold */
             }
 
         /* Media Queries for Responsive Adjustments */
         @media (max-width: 768px) {
-        .container {
+        .container, p, h3 {
             max-width: 90%; /* Allow the container to be more fluid */
             margin: 20px auto; /* Reduced margin for smaller screens */
-        }
-
-        p {
+            padding: 0 15px; /* Adding padding for better edge spacing */
             font-size: 1.2em; /* Slightly smaller font size on smaller devices */
-            margin: 0 0 0.5em 0; /* Reduced bottom margin */
-        }
         }
 
-        @media (max-width: 480px) {
-        .container {
+        h3 {
+            margin-bottom: 0.5em; /* Reduced bottom margin specifically for headers */
+        }
+    }
+
+    @media (max-width: 480px) {
+        .container, p, h3 {
             max-width: 85%; /* Further reduce the max width for very small screens */
             margin: 10px auto; /* Further reduced margin for very small screens */
-        }
-
-        p {
+            padding: 0 10px; /* Adjust padding to ensure content doesn't touch the edges */
             font-size: 1em; /* Even smaller font size for very small devices */
-            margin: 0 0 0.5em 0; /* Keep reduced margin */
         }
-        }
+    }
         </style>
